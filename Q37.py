48d4d0cf7a669223203a0401a906db2c9bd41bd3
Phoebe Liu <poeticphoebe@gmail.com>
2016-02-24 16:57:15 -0500

    Update Q37.py

--- Q37.py
+++ Q37.py
@@ -1,4 +1,6 @@
-a = 1000
+# This script is so slow that I am ashamed.
+
+a = 50000000
 k = range(2,a)
 prime = []
 
@@ -6,18 +8,9 @@ prime = []
 while len(k)>0:
 	i = k[0]
 	k = [g for g in k if g%k[0] != 0]
-	print k
 	prime.append(i)
 	if i**2 > a:
 		break
-		
-prime = prime.remove(2)
-prime = prime.remove(3)
-prime = prime.remove(5)
-prime = prime.remove(7)
-prime = prime.remove(23)
-
-print prime
 
 # test if prime
 def is_prime(n):
@@ -26,11 +19,12 @@ def is_prime(n):
             return False
     return True
 
+
 # test if truncatable
 def is_trun(n):
 	n = map(int, str(n))
-	n2 = [g for g in n if g%2 == 0 and g != 1]
-	if len(n2) == len(n):
+	n2 = [g for g in n if g != 4 and g != 6 and g != 8]
+	if len(n2) == len(n) and len(n2) > 1 and n2[0] != 1 and n2[len(n2)-1] != 1 and n2[0] != 9 and n2[len(n2)-1] != 9:
 		for i in range(1,len(n2)):
 			g = int(''.join(map(str, map(int, n))[i:]))
 			if is_prime(g) is False:
@@ -43,5 +37,5 @@ def is_trun(n):
 		return True
 	return False
 
-g = [g in prime if is_trun(g) is True]
-print g
+k = [g for g in prime if is_trun(g)]
+print k
