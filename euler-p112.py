e1ea37265e9ff95175727e3a54cbfe2a5b732cdb
Phoebe <poeticphoebe@gmail.com>
2026-07-29 15:12:43 +0800

    Add files via upload

---
+++ euler-p112.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 29 15:08:26 2026
+
+@author: phoeb
+"""
+
+#!/usr/bin/env python
+from itertools import *
+
+def pairwise(iterable):
+    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
+def digits(n):
+    return list(map(int, str(n)))
+
+def is_increasing(n):
+    return all(prev <= curr for prev, curr in pairwise(digits(n)))
+
+def is_decreasing(n):
+    return all(prev >= curr for prev, curr in pairwise(digits(n)))
+
+def is_bouncy(n):
+    return not is_increasing(n) and not is_decreasing(n)
+
+def running_total(iterable):
+    total = 0
+    for element in iterable:
+        total += element
+        yield total
+
+def main():
+    nums = count(1)
+    bouncy = running_total(map(lambda n: float(is_bouncy(n)), count(1)))
+    print(next((n for n, b in zip(nums, bouncy) if b / n == 0.99)))
+
+if __name__ == "__main__": main()
